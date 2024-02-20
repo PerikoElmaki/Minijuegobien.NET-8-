f1208397e44e9070137930f6cbe832809b4d3fd8
Pedro Sandoval Martinez <pedro.sandoval.martinez@ces-vegamedia.es>
2024-02-20 12:29:05 +0100

    Label vida faltal escudo y arma

--- VegaJuego/Level1.Designer.cs
+++ VegaJuego/Level1.Designer.cs
@@ -31,6 +31,11 @@
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Level1));
             MovEnemigos = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new PictureBox();
+            labelVida = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // MovEnemigos
@@ -38,24 +43,75 @@
             MovEnemigos.Enabled = true;
             MovEnemigos.Tick += timer1_Tick_1;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = Properties.Resources.salud;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox1.Location = new Point(111, 145);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(43, 39);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // labelVida
+            // 
+            labelVida.AutoSize = true;
+            labelVida.BackColor = Color.Transparent;
+            labelVida.Font = new Font("Showcard Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelVida.ForeColor = Color.Gold;
+            labelVida.Location = new Point(171, 122);
+            labelVida.Name = "labelVida";
+            labelVida.Size = new Size(37, 15);
+            labelVida.TabIndex = 1;
+            labelVida.Text = "Vida";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(212, 169);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(212, 229);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
             // Level1
             // 
             AutoScaleMode = AutoScaleMode.None;
             BackgroundImage = Properties.Resources.FondoFinal;
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(806, 467);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(labelVida);
+            Controls.Add(pictureBox1);
             DoubleBuffered = true;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Level1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "VegaZelda";
             KeyPress += Level1_KeyPress;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Timer MovEnemigos;
+        private PictureBox pictureBox1;
+        private Label labelVida;
+        private Label label1;
+        private Label label2;
     }
 }
